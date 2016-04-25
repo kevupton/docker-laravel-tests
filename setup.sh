67266c551e2609f5523f7aaabfc9c8866d608d0f
Kevin Upton <k_upton@live.com>
2016-04-26 01:43:48 +1000

    removed whitespace

--- setup.sh
+++ setup.sh
@@ -10,9 +10,8 @@ if [ ! -f "laravel/composer.json" ]; then
     composer update
     composer require $PACKAGE_NAME
     if [[ -v PACKAGE_PROVIDER ]]; then
-        echo "$(awk '/'\''providers'\''[^\n]*?\[/ { print; print "'$PACKAGE_PROVIDER',"; next }1' \
+        echo "$(awk '/'\''providers'\''[^\n]*?\[/ { print; print "'$(sed -e 's/\s*//g' <<<${PACKAGE_PROVIDER})',"; next }1' \
             config/app.php)" > config/app.php
-        cat config/app.php
     fi
     if [[ -v SEED_CLASS ]]; then
         echo "$(cat database/seeds/DatabaseSeeder.php | \
