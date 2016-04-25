965df23535fe94099af3f830ec2256335044f622
Kevin Upton <k_upton@live.com>
2016-04-26 01:01:21 +1000

    updates

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ if [ ! -f "laravel/composer.json" ]; then
     composer require $PACKAGE_NAME
     if [[ -v PACKAGE_PROVIDER ]]; then
         echo "$(awk '/'\''providers'\''[^\n]*?\[/ { print; print "'$PACKAGE_PROVIDER',"; next }1' \
-            config/app.php)" > config/app.php;
+            config/app.php)" > config/app.php
+        cat config/app.php
     fi
     if [[ -v SEED_CLASS ]]; then
         echo "$(cat database/seeds/DatabaseSeeder.php | \
