688810bef280311832d5e7a5b56e8ff644985a42
Kevin Upton <k_upton@live.com>
2016-04-28 04:42:26 +1000

    updated to dev master

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ if [ ! -f "laravel/composer.json" ]; then
     composer create-project laravel/laravel
     cd laravel
     composer update
-    composer require $PACKAGE_NAME
+    composer require $PACKAGE_NAME:dev-master
     if [[ -v PACKAGE_PROVIDER ]]; then
         echo "$(awk '/'\''providers'\''[^\n]*?\[/ { print; print "'$(sed -e 's/\s*//g' <<<${PACKAGE_PROVIDER})',"; next }1' \
             config/app.php)" > config/app.php
